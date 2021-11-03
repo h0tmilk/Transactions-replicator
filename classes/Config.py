1b4397631b2694e2f7e35bc89d05404d85427d07
H0tmilk <k.chlt@protonmail.com>
2021-11-04 00:04:59 +0100

    Remove non existent config line parsing

--- classes/Config.py
+++ classes/Config.py
@@ -7,7 +7,6 @@ class Config:
         with open(config_path, "r") as stream:
             try:
                 parsed_conf = yaml.safe_load(stream)
-                self.accounts_folder = parsed_conf['airdrop_autofarmer'][0]['accounts_folder']
                 self.blockchains = parsed_conf['blockchains']
             except yaml.YAMLError as exc:
                 print(exc)
